35db0555615f512cd7f9803ce27c0ff00993c857
Wiseman Lim <wiseman@roundsquare.ai>
2025-02-05 10:13:38 +0900

    Update aws-update-route53.sh

--- aws-update-route53.sh
+++ aws-update-route53.sh
@@ -17,8 +17,8 @@ fi
 #   echo "The hosted zone ID, the domain name you are about to update."
 #   exit
 # fi
-printf '사용하실 도메인을 입력해 주세요.'
-read $domain
+echo '사용하실 도메인을 입력해 주세요.'
+read domain
 
 # Define the JSON payload to send to Route53.
 UPDATE_REQUEST='

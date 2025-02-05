353cf5c6641dd1a332c09cdd5be8653883e0c8e2
Wiseman Lim <wiseman@roundsquare.ai>
2025-02-05 12:51:12 +0900

    Update aws-update-route53.sh

--- aws-update-route53.sh
+++ aws-update-route53.sh
@@ -2,7 +2,7 @@
 # 루트 사용자 변경 (간단하게 sudo -i 실행)
 if [ "$(id -u)" -ne 0 ]; then
     # echo "현재 사용자가 루트가 아닙니다. 루트 권한을 얻기 위해 sudo -i를 실행합니다."
-    $(sudo -i)
+    sudo -i
     if [ $? -ne 0 ]; then
         # echo "sudo -i 실행에 실패했습니다. sudo로 스크립트를 다시 실행합니다."
         exec sudo bash "$0" "$@"
@@ -16,9 +16,10 @@ fi
 # Check params are passed.
 # if [ "$#" -ne 4 ]; then
 #   echo "***ERROR 매개변수가 전달되지 않았습니다. 4개의 매개변수를 전달해 주세요."
-#   echo "AWS key_id, access_key, 그리고 호스트존 ID, 업데이트하려는 도메인 이름입니다."
+   echo "AWS key_id, access_key, 그리고 호스트존 ID, 업데이트하려는 도메인 이름입니다."
 #   exit
 # fi
+
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 aws configure set region ap-northeast-2

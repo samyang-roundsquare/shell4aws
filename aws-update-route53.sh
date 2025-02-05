b27e359e54add065a1f8069c81b92fdc43b0ce96
Wiseman Lim <wiseman@roundsquare.ai>
2025-02-05 09:52:50 +0900

    Update and rename UpdateRoute53.sh to aws-update-route53.sh

--- aws-update-route53.sh
+++ aws-update-route53.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
-# Check params are passed.
-if [ "$#" -ne 2 ]; then
-  echo "***ERROR Parameters not passed. You need to pass two parameters:"
-  echo "The hosted zone ID, the domain name you are about to update."
-  exit
+# 루트 사용자 변경 (간단하게 sudo -i 실행)
+if [ "$(id -u)" -ne 0 ]; then
+    echo "현재 사용자가 루트가 아닙니다. 루트 권한을 얻기 위해 sudo -i를 실행합니다."
+    sudo -i
+    if [ $? -ne 0 ]; then
+        echo "sudo -i 실행에 실패했습니다. sudo로 스크립트를 다시 실행합니다."
+        exec sudo bash "$0" "$@"
+        exit
+    fi
 fi
+
+# Check params are passed.
+# if [ "$#" -ne 2 ]; then
+#   echo "***ERROR Parameters not passed. You need to pass two parameters:"
+#   echo "The hosted zone ID, the domain name you are about to update."
+#   exit
+# fi
+aws configure
+printf '사용하실 도메인을 입력해 주세요.'
+read $domain
 
 # Define the JSON payload to send to Route53.
 UPDATE_REQUEST='
@@ -48,6 +62,6 @@ else
   #echo $UPDATE_REQUEST
   echo $UPDATE_REQUEST > /tmp/ipupdate-request.json
   aws route53 change-resource-record-sets \
-        --hosted-zone-id $1 \
+        --hosted-zone-id $domain \
         --change-batch file:///tmp/ipupdate-request.json
 fi
